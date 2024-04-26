33bdb6ea1b7289f37b4ae7c659aaa755163efefb
202020882 <kmsmomo2@ajou.ac.kr>
2024-04-26 18:04:38 +0900

    kms third
    kms code change

--- assignment_1.m
+++ assignment_1.m
@@ -1,8 +1,13 @@
-current_date = input('현재 날짜 및 시간을 입력하세요 :');
-current_time = input('현재 시간을 입력하세요 :');
-plustime = input ('추가 시간을 입력하세요 :');
-t = datetime([current_date, current_time, 00, 00]);
-ptime = hours(plustime);
-A = t + ptime;
-disp(['현재시간은 ',datestr(t), '입니다.'])
-disp(['나중시간은 ', datestr(A), '입니다.'])
+% 현재 날짜와 시간 입력 받기
+currentYear = input('현재 연도를 입력하세요: ');
+currentMonth = input('현재 월을 입력하세요: ');
+currentDay = input('현재 일을 입력하세요: ');
+currentHour = input('현재 시간을 입력하세요: ');
+
+% 시간 단위의 숫자 입력 받기
+hoursToAdd = input('추가할 시간을 입력하세요: ');
+
+current = datetime(currentYear, currentMonth, currentDay, currentHour, 0, 0);
+% 입력된 현재 날짜와 시각
+newDateTime = datetime(currentYear, currentMonth, currentDay, currentHour, 0, 0) + hours(hoursToAdd);
+% 입력된 현재 시각과 숫자의 시간을 이용하여 날짜와 시간 계산
