27d8be353426ce61a985715a565b0459c42b3469
kjsik <127501452+kjsik@users.noreply.github.com>
2024-04-26 17:30:12 +0900

    junsik
    plz

--- assignment_1.m
+++ assignment_1.m
@@ -3,3 +3,6 @@ current_time = input('현재 시간을 입력하세요 :');
 plustime = input ('추가 시간을 입력하세요 :');
 t = datetime([current_date, current_time, 00, 00]);
 ptime = hours(plustime);
+A = t + ptime;
+disp(['현재시간은 ',datestr(t), '입니다.'])
+disp(['나중시간은 ', datestr(A), '입니다.'])
