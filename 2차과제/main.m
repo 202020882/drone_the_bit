d755f8b66ea631ac287d16c7edd44cdb1b888a9f
unknown <takdonghyun@ajou.ac.kr>
2024-05-26 17:38:59 +0900

    takdonghyun1
    check code

--- 2차과제/main.m
+++ 2차과제/main.m
@@ -1,1 +1,10 @@
-dronethebit
+dronethebit = ryze();
+cameraObj = camera(dronethebit);
+
+takeoff(dronethebit);
+pause(0.5);
+moveleft(dronethebit,5);
+pause(0.5);
+turn(dronethebit,deg2rad(30));
+pause(0.5);
+moveforward(dronethebit,3);
