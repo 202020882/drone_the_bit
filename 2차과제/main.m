38632f6fdcfa49c194f63693093f9e99e2bb2271
kjsik <127501452+kjsik@users.noreply.github.com>
2024-05-26 18:00:24 +0900

    junsik
    jun

--- 2차과제/main.m
+++ 2차과제/main.m
@@ -18,3 +18,12 @@ frame = snapshot(cameraObj);
 pause(0.5);
 imshow(frame);
 pause(0.5);
+
+turn(dronethebit,deg2rad(150));
+pause(0.5);
+moveleft(dronethebit,3);
+pause(0.5);
+turn(dronethebit,deg2rad(120));
+pause(0.5);
+
+land(dronethebit);
